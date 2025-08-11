d89ff1ed53e7ca863e45886fc9ffb75d7f903aea
Nnlllllll <nathannovais020@gmail.com>
2025-08-11 10:03:40 -0300

    Add files via upload

---
+++ Exercicio1_JPanelJButton.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+public class Exercicio1_JPanelJButton {
+    public static void main(String[] args) {
+        // Criação do frame
+        JFrame frame = new JFrame("Exercício 1");
+        frame.setSize(1000, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JPanel painel = new JPanel();
+        painel.setLayout(new FlowLayout(FlowLayout.CENTER, 0,250));
+        painel.setPreferredSize(new Dimension(1000,600));
+        painel.setBackground(Color.GREEN);  // Define cor de fundo
+        painel.setBorder(BorderFactory.createTitledBorder("Painel de Controle")); // Borda com título
+        // Botão
+        JButton botao = new JButton("Clique Aqui");
+        
+        // Adicionando componentes ao frame
+        frame.add(painel);
+        painel.add(botao);
+        
+        // Exibir a janela
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
